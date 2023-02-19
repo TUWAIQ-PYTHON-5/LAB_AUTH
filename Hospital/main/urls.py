4ee73e1a169eb5059a122588fb1a50fc999677aa
NjoudAlhazmi <njoudz19@gmail.com>
2023-02-19 10:14:49 +0300

    solve lap

---
+++ Hospital/main/urls.py
@@ -0,0 +1,19 @@
+from django.urls import path
+from . import views
+
+
+app_name = "url_main"
+
+urlpatterns = [
+    
+    path("" , views.home , name= "home_page"),
+    path("Clinic/details/<clinic_id>/" , views.details , name= "clinic_page"),
+    path("Clinic/new/add" , views.add , name= "add_clinic"),
+    path("Booking/Appointment/<clinic_id>" , views.booking , name= "appointment_page"),
+    path("Clinic/search/", views.search, name="search"),
+    path("Clinic/appointments/", views.appointments, name="appointments"),
+    path("Clinic/update/<clinic_id>", views.update, name="update"),
+    path("Clinic/delete/<clinic_id>", views.delete, name="delete"),
+   
+    # path("Contact/" , views.contact , name= "contact_page"),
+]
