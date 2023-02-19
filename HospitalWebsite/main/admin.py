9abd140ee235c413fb57bc94c400ad168f0ec971
Sara Alotaibi <alotaibisara49@gmail.com>
2023-02-19 10:06:37 +0300

    done

---
+++ HospitalWebsite/main/admin.py
@@ -0,0 +1,15 @@
+
+from django.contrib import admin
+from .models import Clinic,Appointment
+# Register your models here.
+
+
+#to customize the panel
+class ClinicAdmin(admin.ModelAdmin):
+     list_display = ['name','description','feature_image','department','established_at',]
+class AppointmentAdmin(admin.ModelAdmin):
+     list_display = ('user','clinic_name','case_description','patient_age','appointment_datetime','is_attended')
+
+
+admin.site.register(Clinic, ClinicAdmin)
+admin.site.register(Appointment, AppointmentAdmin)
